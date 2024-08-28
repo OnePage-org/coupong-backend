c377d78db779098cee67034292a1ef13b89de2f8
hyoseok <alexization@gmail.com>
2024-08-28 15:53:45 +0900

    [feature/coupang-2] JWT Security 설정을 위한 JwtProvider, WebSecurityConfig, JwtAuthenticationFilter, UserRepository 구현

--- src/main/java/com/onepage/coupong/sign/config/WebSecurityConfig.java
+++ src/main/java/com/onepage/coupong/sign/config/WebSecurityConfig.java
@@ -1,4 +1,86 @@
 package com.onepage.coupong.sign.config;
 
+import com.onepage.coupong.sign.filter.JwtAuthenticationFilter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.io.IOException;
+
+@Configurable
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(CsrfConfigurer::disable)
+                .httpBasic(HttpBasicConfigurer::disable)
+                /* Session을 사용하지 않겠음을 명시 */
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/", "api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/user/**").hasRole("USER")
+                        .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(new FailedAuthenticationEntryPoint()))
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+                return httpSecurity.build();
+    }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        /* 모든 Header, Method, Origin에 대해 허용해주겠다. */
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        /* 모든 패턴에 대해 */
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+    class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+        @Override
+        public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+
+            response.setContentType("application/json");
+            /* 권한 없음 */
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            /* {"code" : "NP", "message" : "No Permission."}
+            * 의 형식으로 응답이 됨 */
+            response.getWriter().write("{\"code\": \"NP\", \"message\": \"No Permission.\"}");
+        }
+    }
 }
--- src/main/java/com/onepage/coupong/sign/provider/JwtProvider.java
+++ src/main/java/com/onepage/coupong/sign/provider/JwtProvider.java
@@ -1,4 +1,63 @@
 package com.onepage.coupong.sign.provider;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
+@Component
 public class JwtProvider {
+
+    @Value("${jwt.secret.key}")
+    private String secretKey;
+
+    /* JWT를 만들어주는 메서드 */
+    public String create(String username) {
+
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+        /* JWT에서 VERIFY SIGNATURE에 해당하는 key값을 만들어준다 */
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        String jwt = Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                /* setSubject() -> JWT PAYLOAD에서 "sub"에 해당하는 내용
+                 * setIssuedAt() -> JWT PAYLOAD에서 "iat"에 해당하는 내용
+                 * setExpiration() -> 만료 시간 */
+                .setSubject(username).setIssuedAt(new Date()).setExpiration(expiredDate)
+                .compact();
+
+        return jwt;
+    }
+
+    /* JWT 검증 메서드 */
+    public String validate(String jwt) {
+
+        String subject = null;
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        try {
+
+            Claims claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jwt)
+                    .getBody();
+
+            subject = claims.getSubject();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return subject;
+    }
 }
