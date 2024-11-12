48cab6cec868ce5cc3ad18bc55e3d6c80004c458
dhfkdlsj <dhfkdlsj1@gmail.com>
2024-11-12 15:07:58 +0900

    [refactoring/coupong-27] dto 추가

---
+++ src/main/java/com/onepage/coupong/business/leaderboard/dto/SseEmitterDto.java
@@ -0,0 +1,6 @@
+package com.onepage.coupong.business.leaderboard.dto;
+
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+public record SseEmitterDto(SseEmitter sseEmitter) {
+}
